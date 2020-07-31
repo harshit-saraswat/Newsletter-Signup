787fff27a9735f7374d4afdcad998c9073b215ed
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-01 01:15:32 +0530

    Adding to mail list functionality added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+const secrets = require("./secrets");
+const { response } = require('express');
+
 app = express();
 
 app.use(express.static("static"));
@@ -18,7 +21,39 @@ app.post("/", function (req, res) {
   var fname = req.body.fname;
   var lname = req.body.lname;
   var email = req.body.email;
-  console.log(fname+lname+email);
+
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields:{
+        FNAME:fname,
+        LNAME:lname
+      }
+    }]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us17.api.mailchimp.com/3.0/lists/" + secrets['list-id'],
+    method: "POST",
+    headers: {
+      "Authorization": "harshit-saraswat " + secrets['api-key']
+    },
+    body: jsonData
+  };
+
+  request(options, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      console.log(response.statusCode);
+    }
+  });
+
+
 });
 
 app.listen(3000, function () {
