5f2700fb3bf2487ab3d51b132f4c072f1adb30fb
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-07-31 21:01:22 +0530

    UI and Form Data collection done

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-app=express();
+app = express();
+
+app.use(express.static("static"));
 
 app.use(bodyParser.urlencoded(
-  { extended:true }
+  { extended: true }
 ));
 
-app.get("/",function (req,res){
-    res.sendFile(__dirname+"/index.html");
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen(3000,function (){
-    console.log("Server started at port 3000");
-})
+app.post("/", function (req, res) {
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var email = req.body.email;
+  console.log(fname+lname+email);
+});
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+});
